6df74028ad9b0f46e9c68c1f6e0f40c0c0ca0b18
Eduard Lofitskyi <edosgk@gmail.com>
2016-03-05 13:04:32 +0200

    add state entity

---
+++ amberpluto-repository/src/main/java/com/lofitskyi/entity/State.java
@@ -0,0 +1,48 @@
+package com.lofitskyi.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table (name = "state")
+public class State {
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    private long id;
+
+    @Column(name = "name", nullable = false, length = 25)
+    private String name;
+
+    @Column(name = "short_name", nullable = false, length = 3)
+    private String shortName;
+
+    public State() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+}
