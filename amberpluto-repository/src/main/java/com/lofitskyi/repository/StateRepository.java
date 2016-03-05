a0ef159da3cae4b0fd503879b2b31fa882a9d669
Eduard Lofitskyi <edosgk@gmail.com>
2016-03-05 22:35:50 +0200

    ccnfigurated db and jpa

---
+++ amberpluto-repository/src/main/java/com/lofitskyi/repository/StateRepository.java
@@ -0,0 +1,7 @@
+package com.lofitskyi.repository;
+
+import com.lofitskyi.entity.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State, Long>{
+}
