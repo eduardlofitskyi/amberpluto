7ee1879b69a07f19bed55c3b7f6676618c2e3bd9
Eduard Lofitskyi <edosgk@gmail.com>
2016-03-07 11:43:00 +0200

    annotated stateRepository

--- amberpluto-repository/src/main/java/com/lofitskyi/repository/StateRepository.java
+++ amberpluto-repository/src/main/java/com/lofitskyi/repository/StateRepository.java
@@ -2,6 +2,8 @@ package com.lofitskyi.repository;
 
 import com.lofitskyi.entity.State;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StateRepository extends JpaRepository<State, Long>{
 }
