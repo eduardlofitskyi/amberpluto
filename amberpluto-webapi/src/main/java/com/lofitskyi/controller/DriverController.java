828dc0c60e862e1e6e53cf2159f62e7bed55cfe3
Eduard Lofitskyi <edosgk@gmail.com>
2016-04-05 12:46:33 +0300

    add bus controller

---
+++ amberpluto-webapi/src/main/java/com/lofitskyi/controller/DriverController.java
@@ -0,0 +1,23 @@
+package com.lofitskyi.controller;
+
+import com.lofitskyi.entity.Driver;
+import com.lofitskyi.service.DriverService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rest/driver")
+public class DriverController {
+
+    @Autowired
+    private DriverService service;
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    public List<Driver> getAllDrivers(){
+        return service.getAll();
+    }
+}
