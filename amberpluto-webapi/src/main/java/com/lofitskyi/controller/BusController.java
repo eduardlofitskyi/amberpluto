e0fec4537eda0d5ae35e577e65979bccc514be9f
Eduard Lofitskyi <edosgk@gmail.com>
2016-04-05 12:25:56 +0300

    add bus controller

---
+++ amberpluto-webapi/src/main/java/com/lofitskyi/controller/BusController.java
@@ -0,0 +1,24 @@
+package com.lofitskyi.controller;
+
+import com.lofitskyi.entity.Bus;
+import com.lofitskyi.entity.City;
+import com.lofitskyi.service.BusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rest/bus")
+public class BusController {
+
+    @Autowired
+    private BusService service;
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    public List<Bus> getAllBuses(){
+        return service.getAll();
+    }
+}
